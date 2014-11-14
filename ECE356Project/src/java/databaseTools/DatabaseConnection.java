132721bd8691f67a148112463cda1900211a7924
Bo Zhao <b26zhao@uwaterloo.ca>
2014-11-14 00:38:24 -0500

    patients start
    just starting out the patients page. assuming a properly formatted ohip
    number is passed in after the login page clears it. also a bunch of
    unused stuff so far

---
+++ ECE356Project/src/java/databaseTools/DatabaseConnection.java
@@ -0,0 +1,33 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package databaseTools;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+/**
+ *
+ * @author Bo
+ */
+public class DatabaseConnection {
+    public static final String url = "jdbc:mysql://eceweb.uwaterloo.ca:3306/";
+    public static final String user = "user_b26zhao";
+    public static final String pwd = "user_b26zhao";
+    
+    //private static final Connection = DriverManager.getConnection(url, user, pwd);
+    
+    public static void testConnection()
+            throws ClassNotFoundException, SQLException {
+
+        Statement stmt;
+        Connection con;
+        Class.forName("com.mysql.jdbc.Driver");
+        con = DriverManager.getConnection(url, user, pwd);
+        stmt = con.createStatement();
+        con.close();
+    }
+}
